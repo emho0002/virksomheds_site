072b86a7ac0922974bfff8088defc2ff18ef6cd7
Miv Anna <mibe0002@stud.kea.dk>
2023-12-13 16:19:27 +0100

    ændre alle billeder til webp - alle sites er færdige

--- item_sort.js
+++ item_sort.js
@@ -3,14 +3,33 @@
 // Gallery Item filter" samt. chatgpt.
 //
 
-// Click event for elements with class 'filter' and 'a' tag inside
 document.querySelectorAll(".filter a").forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
     var a = link.getAttribute("href").substr(1);
-    document.querySelectorAll(".items a").forEach((itemsLink) => {
-      if (!itemsLink.classList.contains(a) && a !== "all") itemsLink.classList.add("hide");
-      else itemsLink.classList.remove("hide");
+
+    document.querySelectorAll(".items .item").forEach((item) => {
+      item.classList.remove("hide");
     });
+
+    if (a !== "all") {
+      document.querySelectorAll(".items .item").forEach((item) => {
+        if (!item.querySelector(`[class*="${a}"]`)) {
+          item.classList.add("hide");
+        }
+      });
+    }
+
+    const visibleItems = document.querySelectorAll(".items .item:not(.hide)");
+    if (visibleItems.length > 0) {
+      document.querySelector(".items").classList.add("show-items");
+    } else {
+      document.querySelector(".items").classList.remove("show-items");
+    }
+
+    // document.querySelectorAll(".items a").forEach((itemsLink) => {
+    //   if (!itemsLink.classList.contains(a) && a !== "all") itemsLink.classList.add("hide");
+    //   else itemsLink.classList.remove("hide");
+    // });
   });
 });
